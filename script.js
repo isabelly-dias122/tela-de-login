cfa1007c32cf31d6ba92cc373c260f0b4509520c
Maria Clara Ferraz Ferreira <00001124516530sp@aluno.educacao.sp.gov.br>
2026-06-24 10:33:06 -0300

    Implementa validação de credenciais do usuário

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 document.getElementById("login-form").addEventListener("submit", function(event) {
     event.preventDefault(); // Impede o envio do formulário
 
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("password").value;
-
-    // Usuário e senha de teste
-    const emailCorreto = "admin@email.com";
-    const senhaCorreta = "123456";
-
-    if (email === emailCorreto && senha === senhaCorreta) {
-        alert("Login realizado com sucesso!");
-
-        // Redireciona para outra página
-        window.location.href = "home.html";
-    } else {
-        alert("E-mail ou senha incorretos!");
-    }
-});
+})
