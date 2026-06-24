0a6a7b33b79baa0d372abdb51c18bebcf88f6d3d
Maria Clara Ferraz Ferreira <00001124516530sp@aluno.educacao.sp.gov.br>
2026-06-24 10:38:34 -0300

    Implementa variaveis para os campos email e senha

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.getElementById("login-form").addEventListener("submit", function(event) {
     event.preventDefault(); // Impede o envio do formulário
 
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("password").value;
+    
 })
